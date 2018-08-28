3587f99cc1274cd2f1d38e7465dd65c643291678
chauhanreshma16 <creshama402@gmail.com>
2018-08-28 18:25:43 +0530

    chass

---
+++ chass.php
@@ -0,0 +1,112 @@
+<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title> Title </title>
+	<link rel="stylesheet" type="text/css" href="css/chassstyle.css">
+</head>
+
+<body>
+    <div class="img"></div>
+	<div class="board" >
+		
+<!--		row-->
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+<!--		end row-->
+				
+<!--		row-->
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+<!--		end row-->
+				
+<!--		row-->
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+<!--		end row-->
+				
+<!--		row-->
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+<!--		end row-->
+				
+<!--		row-->
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+<!--		end row-->
+				
+<!--		row-->
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+<!--		end row-->
+				
+<!--		row-->
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+<!--		end row-->
+				
+<!--		row-->
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+		<div class="topleft1"></div>
+		<div class="topleft"></div>
+<!--		end row-->
+				
+				
+</div>
+
+</body>
+
+</html>
+
+</html>
